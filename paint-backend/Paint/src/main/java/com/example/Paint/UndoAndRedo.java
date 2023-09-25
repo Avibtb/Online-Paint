fb53cc593346300b8b42f864b3ecdbdba11df217
Avibtb <avi.singh.iit01@gmail.com>
2023-09-25 22:38:08 +0530

    Added insertShape and convertMapToJson.

--- paint-backend/Paint/src/main/java/com/example/Paint/UndoAndRedo.java
+++ paint-backend/Paint/src/main/java/com/example/Paint/UndoAndRedo.java
@@ -1,5 +1,12 @@
 package com.example.Paint;
 
+import com.example.Paint.shapes.Shape;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.util.HashMap;
 
 public class UndoAndRedo {
@@ -11,7 +18,6 @@ public class UndoAndRedo {
     public static String[] arr = new String[10];
 
     //undo function
-
     public static HashMap<Long, String[]> undo() {
         int len = undo_hashmap.keySet().size();
         Long[] keys_arr = undo_hashmap.keySet().toArray(new Long[len]);
@@ -33,5 +39,42 @@ public class UndoAndRedo {
         redo_hashmap.put(keys_arr[len-1],tempArr);
         undo_hashmap.remove(keys_arr[len-1]);
         return undo_hashmap;
+    }
+
+    public void insertShape(Shape shape){
+        String[] shapeAttributes = new String[10];
+        shapeAttributes[0] = shape.getType();
+        shapeAttributes[1] = shape.getFill();
+        shapeAttributes[2] = shape.getBorder();
+        shapeAttributes[3] = String.valueOf(shape.getX1());
+        shapeAttributes[4] = String.valueOf(shape.getY1());
+        shapeAttributes[5] = String.valueOf(shape.getX2());
+        shapeAttributes[6] = String.valueOf(shape.getY2());
+        shapeAttributes[7] = String.valueOf(shape.getRotation_angle());
+        if(shape.isExist())
+            shapeAttributes[8] = "true";
+        else
+            shapeAttributes[8] = "false";
+        shapeAttributes[9] = String.valueOf(shape.getPrevid());
+        undo_hashmap.put(shape.getId(),shapeAttributes);
+    }
+
+    // To convert a given hashmap into JSON file
+    public String convertMapToJson(HashMap<Long,String[]>map){
+        // make an instance object of the mapper class
+        ObjectMapper mapper = new ObjectMapper();
+        // initialize the json file as an empty string
+        String json = "";
+        try{
+            //Convert Map to JSON
+            json = mapper.writeValueAsString(map);
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        }catch (JsonMappingException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return  json;
     }
 }
